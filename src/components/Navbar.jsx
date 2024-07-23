1aea2641993aebbc18f3273958b7fdc2313ad322
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-23 13:25:29 +0530

    logout button and i'ts logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success text-black">
@@ -28,17 +34,40 @@ const Navbar = () => {
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/login">
+              {localStorage.getItem("authToken") ? (
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    My Order
+                  </Link>
+                </li>
+              ) : (
+                ""
+              )}
+            </ul>
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <Link className="btn bg-white text-success mx-1" to="/login">
                   Login
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/createuser">
+
+                <Link
+                  className="btn bg-white text-success mx-1"
+                  to="/createuser"
+                >
                   Signup
                 </Link>
-              </li>
-            </ul>
+              </div>
+            ) : (
+              <div>
+                <div className="btn bg-white text-success mx-2">Cart</div>
+                <div
+                  className="btn bg-white text-danger mx-2"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
